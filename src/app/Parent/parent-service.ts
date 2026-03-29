1c99c8b2c847e05f97e28ecc4b0b325324fd4643
NidhiPradh <nidhipradhan54@gmail.com>
2026-03-29 23:52:25 +0530

    add product api call

--- src/app/Parent/parent-service.ts
+++ src/app/Parent/parent-service.ts
@@ -1,15 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+//import { environment } from '';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ParentService {
   testData: string = 'Hello from ParentService!';
-  //private baseUrl = 'https://localhost:7286/api/Master/fetch-save-drug?brandname=Advil';
+  private baseUrl = environment.apiUrl;
+  
   constructor(private http: HttpClient) { }
+  
   getProducts(): Observable<any> {
-    return this.http.get(`https://localhost:7286/api/Product/get-all-products`);
+    return this.http.get(`${this.baseUrl}/Product/get-all-products`);    
+  }
+  addProduct(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/Product/create-product`, data);
   }
 }
--- src/app/Parent/parent.component.ts
+++ src/app/Parent/parent.component.ts
@@ -15,7 +15,6 @@ export class ParentComponent implements OnInit {
         // const testData = this._parentService.testData;
         // console.log('Test Data from Service:', testData);
         this.getProducts();
-        console.log('API is getting called');
     }
     getProducts() {
         this._parentService.getProducts().subscribe({
@@ -34,47 +33,91 @@ export class ParentComponent implements OnInit {
         //     console.log('Data received from service:', data);
         // }
     }
+    // isSignup = false;
+    // formData: any = null;
+    // sumresult: number = 0;
+    // inputOne: number | undefined;
+    // inputTwo: number | undefined;
+    // message: string = "";
+    // handleForm(data: any) {
+    //     console.log('Received data from child:', data);
+    //     this.formData = data;
+    //     if (this.isSignup) {
+    //         console.log('Signup Data:', data);
+    //     } else {
+    //         console.log('Signin Data:', data);
+    //     }
 
-    isSignup = false;
-    formData: any = null;
-    sumresult: number = 0;
-    inputOne: number | undefined;
-    inputTwo: number | undefined;
-    message: string = "";
-    handleForm(data: any) {
-        console.log('Received data from child:', data);
-        this.formData = data;
-        if (this.isSignup) {
-            console.log('Signup Data:', data);
-        } else {
-            console.log('Signin Data:', data);
-        }
+    //     //     name: string = 'John Doe';
+    //     //     inputValue: string = '';
+    //     //     receivedMessage: string = '';
 
-        //     name: string = 'John Doe';
-        //     inputValue: string = '';
-        //     receivedMessage: string = '';
+    //     //     constructor() { }
+    //     //     ngOnInit(): void {
+    //     //         this.name = '';
+    //     //     }
+    //     //     onSubmit(): void {
+    //     //         this.name = this.inputValue;
+    //     //         //debugger;
+    //     //         console.log('Submitted name:', this.name);
+    //     //     }
+    //     //     receivedData: string = '';
 
-        //     constructor() { }
-        //     ngOnInit(): void {
-        //         this.name = '';
-        //     }
-        //     onSubmit(): void {
-        //         this.name = this.inputValue;
-        //         //debugger;
-        //         console.log('Submitted name:', this.name);
-        //     }
-        //     receivedData: string = '';
+    //     //   receiveData(data: string): void {
+    //     //     this.receivedData = data;
+    //     //     console.log('From child:', data);
+    // }
+    // calculateSum(): void {
+    //     if (this.inputOne !== undefined && this.inputTwo !== undefined) {
+    //         this.sumresult = Number(this.inputOne) + Number(this.inputTwo);
+    //         console.log('Sum:', this.sumresult);
+    //     } else {
+    //         console.log('Please enter valid numbers for both inputs.');
+    //     }
+    // }
 
-        //   receiveData(data: string): void {
-        //     this.receivedData = data;
-        //     console.log('From child:', data);
+    //product code
+    product: any = {
+    productID: 0,
+    productName: '',
+    productCategory: '',
+    recievedDate: new Date(),
+    productStatus: '',
+    batchNumber: '',
+    manufactureDate: new Date(),
+    expiryDate: '',
+    createdBy: null,
+    createdOn: new Date(),
+    updatedBy: null,
+    updatedOn: new Date()
+  };
+
+  successMessage = '';
+  errorMessage = '';
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form Data:', this.product);
+        this._parentService.addProduct(this.product).subscribe({
+            next: (res) => {
+                console.log('Product added successfully:', res);
+        },
+            error: (err) => {
+                console.error('Error adding product:', err);
+                this.errorMessage = 'Error adding product.';
+                this.successMessage = '';
+            }
+        });
+      // 👉 Call your API here
+      // this.productService.createProduct(this.product).subscribe(...)
+
+      this.successMessage = 'Product saved successfully!';
+      this.errorMessage = '';
+      form.resetForm();
+    } else {
+      this.errorMessage = 'Please fill all required fields.';
+      this.successMessage = '';
     }
-    calculateSum(): void {
-        if (this.inputOne !== undefined && this.inputTwo !== undefined) {
-            this.sumresult = Number(this.inputOne) + Number(this.inputTwo);
-            console.log('Sum:', this.sumresult);
-        } else {
-            console.log('Please enter valid numbers for both inputs.');
-        }
-    }
+  }
+    //
 }
