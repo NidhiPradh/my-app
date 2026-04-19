a8b9207b73fb36ab6ab2a9de0d8e66526e19a3be
NidhiPradh <nidhipradhan54@gmail.com>
2026-04-20 00:31:58 +0530

    lifecyle hook

--- src/app/samples/sample-list/sample-list.component.ts
+++ src/app/samples/sample-list/sample-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { Sample } from '../models/sample.model';
 import { SampleService } from '../services/sample-service';
 import { ChangeDetectorRef } from '@angular/core';
@@ -10,13 +10,35 @@ import { Router } from '@angular/router';
   templateUrl: './sample-list.component.html',
   styleUrl: './sample-list.component.css',
 })
-export class SampleListComponent implements OnInit {
+export class SampleListComponent implements OnInit,OnChanges,DoCheck,AfterContentInit,
+AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy {
   samples: Sample[] = [];
   constructor(
     private sampleService: SampleService,
     private cdr: ChangeDetectorRef,
     private router: Router
   ) { }
+  ngOnDestroy(): void {
+    console.log('ng destroy called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ng after view checked called');
+  }
+  ngAfterViewInit(): void {
+    console.log('ng after view init called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ng after content checked called');
+  }
+  ngAfterContentInit(): void {
+    console.log('ng after content init called');
+  }
+  ngDoCheck(): void {
+    console.log('ng do check called');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ng on changes called');
+  }
   ngOnInit(): void {
     this.loadSamples();
   }
